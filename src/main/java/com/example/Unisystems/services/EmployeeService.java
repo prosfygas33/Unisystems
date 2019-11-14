7f840b269d7c6ce2c233c1fad5f97f4cabcd1433
filekkas <flekkas@hotmail.gr>
2019-11-14 21:45:04 +0200

    Change controller done
    Change controller done

--- src/main/java/com/example/Unisystems/services/EmployeeService.java
+++ src/main/java/com/example/Unisystems/services/EmployeeService.java
@@ -7,64 +7,179 @@ import com.example.Unisystems.model.Employee;
 import com.example.Unisystems.model.Error;
 import com.example.Unisystems.model.EmployeeResponse;
 import com.example.Unisystems.model.GenericResponse;
-import com.example.Unisystems.repositories.EmployeeRepository;
+import com.example.Unisystems.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
     @Autowired
-    EmployeeMapper employeeMapper;
-
+    private EmployeeRepository repository;
     @Autowired
-    EmployeeRepository employeeRepository;
-
+    private CompanyRepository companyRepository;
     @Autowired
-    SearchEmployeeStrategyFactory searchEmployeeStrategyFactory;
+    private BusinessUnitRepository businessUnitRepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    @Autowired
+    private UnitRepository unitRepository;
+    @Autowired
+    private EmployeeMapper mapper;
+    @Autowired
+    private SearchEmployeeStrategyFactory factory;
 
-    public List<EmployeeResponse> getEmployees(){
-        Iterable<Employee> retrievedEmployees = employeeRepository.findAll();
+
+    public List<EmployeeResponse> getAllEmployees() {
+        Iterable<Employee> retrieveEmployees = repository.findAll();
         List<EmployeeResponse> employees = new ArrayList<>();
-
-        for ( Employee employee : retrievedEmployees ){
-            employees.add(employeeMapper.mapEmployeeResponseFromEmployee(employee));
+        for(Employee employee: retrieveEmployees){
+            employees.add(mapper.mapEmployeeResponseFromEmployee(employee));
         }
-
         return employees;
     }
 
-    /*public GenericResponse<List<EmployeeResponse>> getEmployeesById(String searchCriteria, long id){
-        Iterable<Employee> retrievedEmployees = employeeRepository.findAll();
-        List<EmployeeResponse> employees; //= new ArrayList<>();
-
-        SearchEmployeeStrategy strategy = searchEmployeeStrategyFactory.makeStrategyForCriteria(searchCriteria);
-        employees = employeeMapper.mapAllEmployees(strategy.execute( id,retrievedEmployees));
-
-        return GenericResponse<>(employeesToReturn);
-    }*/
-
-    public GenericResponse<List<EmployeeResponse>> getEmployeesById(String searchCriteria, long id) {
-        Iterable<Employee> employees = employeeRepository.findAll();
-        List<EmployeeResponse> employeesToReturn = new ArrayList<>();
-
-        if (searchCriteria.equals("employees")) {
-
-            if (!employeeRepository.findById(id).isPresent())
-                return new GenericResponse<>(new Error(0, "Wrong Input", "Employee with id " + id + " does not exist"));
-
-            for (Employee employee : employees) {
-                if (employee.getId() == id) {
-                    employeesToReturn.add(employeeMapper.mapEmployeeResponseFromEmployee(employee));
-                }
-            }
+    public List<EmployeeResponse> getAllEmployeesById(Long id) {
+        Iterable<Employee> retrieveEmployees = repository.findAll();
+        List<EmployeeResponse> employees = new ArrayList<>();
+        for(Employee employee: retrieveEmployees){
+            if(id == employee.getId())
+                employees.add(mapper.mapEmployeeResponseFromEmployee(employee));
         }
-        return new GenericResponse<>(employeesToReturn);
+        return employees;
+    }
+
+    public GenericResponse<List<EmployeeResponse>> getAllEmployeesByCriteria(String criteria, Long id) {
+        Iterable<Employee> retrieveEmployees = repository.findAll();
+        List<EmployeeResponse> employees;// = new ArrayList<>();
+        SearchEmployeeStrategy strategy = factory.makeStrategyForCriteria(criteria);
+        if(strategy == null)  return new GenericResponse<>(new Error(0,"Wrong Input", "This " +  criteria + " do not exist"));
+
+        employees = mapper.mapAllEmployees(strategy.execute(id, retrieveEmployees));
+
+        if(employees.isEmpty())  return new GenericResponse<>(new Error(0,"Not Found", "No employee record exist for given id " + id));
+
+        return new GenericResponse<>(employees);
+    }
+
+    /*
+
+    public EmployeeDetailsResponse updateEmployee(Employee employee, Long id) {
+        Optional<Employee> employeeOptional  = repository.findById(id);
+
+        employee.setId(id);
+        employee.setCompany(getCompany(employee.getCompanyName()));
+        employee.setBusinessUnit(getBusinessUnit(employee.getBusinessUnitName()));
+        employee.setDepartment(getDepartment(employee.getDepartmentName()));
+        employee.setUnit(getUnit(employee.getUnitName()));
+        repository.save(employee);
+        return mapper.mapEmployeeDetailsResponse(employee);
+    }
+
+    public String deleteEmployee(Long id) {
+        repository.deleteById(id);
+        return "OK";
     }
 
 
+     */
 
+/*
+    private Company getCompany(String name){
+        Iterable<Company> retrieveCompanyes = companyRepository.findAll();
+        for(Company company: retrieveCompanyes){
+            if(name.equals(company.getName()))
+                return company;
+        }
+        return null;
+    }
+
+    private BusinessUnit getBusinessUnit(String name){
+        Iterable<BusinessUnit> retrieveBusinessUnits = businessUnitRepository.findAll();
+        for(BusinessUnit businessUnit: retrieveBusinessUnits){
+            if(name.equals(businessUnit.getName()))
+                return businessUnit;
+        }
+        return null;
+    }
+
+    private Department getDepartment(String name){
+        Iterable<Department> retrieveDepartments = departmentRepository.findAll();
+        for(Department department: retrieveDepartments){
+            if(name.equals(department.getName()))
+                return department;
+        }
+        return null;
+    }
+
+    private Unit getUnit(String name){
+        Iterable<Unit> retrieveUnits = unitRepository.findAll();
+        for(Unit unit: retrieveUnits){
+            if(name.equals(unit.getName()))
+                return unit;
+        }
+        return null;
+    }
+
+ */
+
+
+    /*
+    public List<EmployeeResponse> getAllEmployeesByCriteria(String searchCriteria, Long id) {
+        Iterable<Employee> retrieveEmployees = repository.findAll();
+        List<EmployeeResponse> employees = new ArrayList<>();
+
+        if(searchCriteria.equalsIgnoreCase("company"))
+            //return  new SearchEmployeeByCompanyStrategy();//employees = searchInCompany(retrieveEmployees, id);
+        if(searchCriteria.equalsIgnoreCase("businessUnit"))
+            //employees = searchInBusinessUnit(retrieveEmployees, id);
+        if(searchCriteria.equalsIgnoreCase("department"))
+            //employees = searchInDepartment(retrieveEmployees, id);
+        if(searchCriteria.equalsIgnoreCase("unit"))
+            //employees = searchInUnit(retrieveEmployees, id);
+        return employees;
+    }
+*/
+
+    /*
+    private List<EmployeeResponse> searchInUnit(Iterable<Employee> retrieveEmployees, Long id) {
+        List<EmployeeResponse> employees = new ArrayList<>();
+        for(Employee employee: retrieveEmployees){
+            if(id == employee.getUnit().getId())
+                employees.add(mapper.mapEmployeeResponse(employee));
+        }
+        return employees;
+    }
+
+    private List<EmployeeResponse> searchInDepartment(Iterable<Employee> retrieveEmployees, Long id) {
+        List<EmployeeResponse> employees = new ArrayList<>();
+        for(Employee employee: retrieveEmployees){
+            if(id == employee.getDepartment().getId())
+                employees.add(mapper.mapEmployeeResponse(employee));
+        }
+        return employees;
+    }
+
+    private List<EmployeeResponse> searchInBusinessUnit(Iterable<Employee> retrieveEmployees, Long id) {
+        List<EmployeeResponse> employees = new ArrayList<>();
+        for(Employee employee: retrieveEmployees){
+            if(id == employee.getBusinessUnit().getId())
+                employees.add(mapper.mapEmployeeResponse(employee));
+        }
+        return employees;
+    }
+
+    private List<EmployeeResponse> searchInCompany(Iterable<Employee> retrieveEmployees, Long id) {
+        List<EmployeeResponse> employees = new ArrayList<>();
+        for(Employee employee: retrieveEmployees){
+            if(id == employee.getCompany().getId())
+                employees.add(mapper.mapEmployeeResponse(employee));
+        }
+        return employees;
+    }
+*/
 }
