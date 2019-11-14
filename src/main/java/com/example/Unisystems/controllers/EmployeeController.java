993a91397ed82d688af792dafa99afca2e3ab0bd
APE\kostis21 <kostas.giannopoulos@yahoo.gr>
2019-11-14 15:28:33 +0200

    Add Employees

--- src/main/java/com/example/Unisystems/controllers/EmployeeController.java
+++ src/main/java/com/example/Unisystems/controllers/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,6 +25,8 @@ public class EmployeeController {
     public GetAllEmployees getAllEmployees(){
         return new GetAllEmployees(employeeService.getEmployees());
     }
+
+
 
     @GetMapping("{searchCriteria}/{id}")
     public ResponseEntity getEmployeesById(@PathVariable String searchCriteria, @PathVariable long id) {
