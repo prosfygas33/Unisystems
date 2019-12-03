44a80a467650d08c815cebfe4c5c483b124b91fd
gerasimos <gerikool@hotmail.com>
2019-12-03 09:43:12 +0200

    loging out
    Fix logOut

--- src/main/java/com/example/Unisystems/Security/SecurityConfiguration.java
+++ src/main/java/com/example/Unisystems/Security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -36,9 +37,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/**").authenticated()
+                .antMatchers("/**").hasAnyRole("EMPLOYEE","ADMIN")
                 .and()
-                .httpBasic();
+                //.httpBasic()
+                .formLogin()
+                .and()
+                .logout();//.logoutRequestMatcher(new AntPathRequestMatcher("/signOut")).logoutSuccessUrl("/");
     }
 
     @Bean
