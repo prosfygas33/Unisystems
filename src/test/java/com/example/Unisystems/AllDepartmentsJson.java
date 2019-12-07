05b5ffe3d51e54bb703e9804760e220dcb159f87
George Ntomoras <gntomor@gmail.com>
2019-12-07 15:11:44 +0200

    committing AcceptanceTests for Unit and Department
    Implemented Acceptance Tests for the Unit and Deparment endpoints

---
+++ src/test/java/com/example/Unisystems/AllDepartmentsJson.java
@@ -0,0 +1,44 @@
+package com.example.Unisystems;
+
+public class AllDepartmentsJson {
+    public static String Json="{\n" +
+            "    \"departments\": [\n" +
+            "        {\n" +
+            "            \"id\": 1,\n" +
+            "            \"name\": \"Human Resources\",\n" +
+            "            \"businessUnit\": {\n" +
+            "                \"id\": 1,\n" +
+            "                \"name\": \"BusinessUnitA\",\n" +
+            "                \"company\": {\n" +
+            "                    \"id\": 1,\n" +
+            "                    \"name\": \"UniSystems\"\n" +
+            "                }\n" +
+            "            }\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 2,\n" +
+            "            \"name\": \"IT Services\",\n" +
+            "            \"businessUnit\": {\n" +
+            "                \"id\": 1,\n" +
+            "                \"name\": \"BusinessUnitA\",\n" +
+            "                \"company\": {\n" +
+            "                    \"id\": 1,\n" +
+            "                    \"name\": \"UniSystems\"\n" +
+            "                }\n" +
+            "            }\n" +
+            "        },\n" +
+            "        {\n" +
+            "            \"id\": 3,\n" +
+            "            \"name\": \"Management\",\n" +
+            "            \"businessUnit\": {\n" +
+            "                \"id\": 2,\n" +
+            "                \"name\": \"BusinessUnitB\",\n" +
+            "                \"company\": {\n" +
+            "                    \"id\": 1,\n" +
+            "                    \"name\": \"UniSystems\"\n" +
+            "                }\n" +
+            "            }\n" +
+            "        }\n" +
+            "    ]\n" +
+            "}";
+}
